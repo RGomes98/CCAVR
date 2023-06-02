0e8d5319a38ac0f54689b0a3ab42523af39171c4
Renan Gomes <renango2015@gmail.com>
2023-06-02 04:29:35 -0300

    Update Contact form

--- src/components/HomeComponents/Contact.tsx
+++ src/components/HomeComponents/Contact.tsx
@@ -1,99 +1,195 @@
-import { useRef } from 'react';
+import { validateTelephone } from '@/utils/validateTelephone';
+import { useRef, useState } from 'react';
+import { cities } from '@/data/cities';
+
 import styles from '../../stylesheets/components/HomeComponentsStyles/Contact.module.scss';
 import ReCAPTCHA from 'react-google-recaptcha';
 
+type Form = {
+  name: string;
+  email: string;
+  telephone: string;
+  city: string;
+  subject: string;
+  content: string;
+};
+
+type FormError = {
+  name: boolean;
+  telephone: boolean;
+  content: boolean;
+};
+
+type ChangeInput = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
+
 export const Contact: React.FC = () => {
+  const [formErrors, setFormErrors] = useState<FormError>({
+    name: false,
+    telephone: false,
+    content: false,
+  });
+
+  const [formData, setFormData] = useState<Form>({
+    name: '',
+    email: '',
+    telephone: '',
+    city: '',
+    subject: '',
+    content: '',
+  });
+
   const reCAPTCHARef = useRef<ReCAPTCHA>(null);
+
+  const handleChange = (e: React.ChangeEvent<ChangeInput>) => {
+    const { value } = e.target;
+    const { id } = e.target;
+    const telephoneLength = 11;
+
+    if (id !== 'telephone') setFormData((prev) => ({ ...prev, [id]: value }));
+    if (id === 'telephone' && value.length <= telephoneLength) {
+      setFormData((prev) => ({ ...prev, [id]: value }));
+    }
+  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    return console.log('Blocked');
+    return console.log('Block Email!');
 
-    const {
-      contact_name,
-      contact_email,
-      contact_telephone,
-      contact_city,
-      contact_subject,
-      contact_content,
-    } = e.currentTarget;
+    const { name, email, telephone, city, subject, content } = formData;
+    setFormErrors({ name: false, telephone: false, content: false });
+    let hasError = false;
+
+    if (!name.trim()) {
+      hasError = true;
+      setFormErrors((prev) => ({ ...prev, name: true }));
+    }
+
+    if (!content.trim()) {
+      hasError = true;
+      setFormErrors((prev) => ({ ...prev, content: true }));
+    }
+
+    if (!validateTelephone(telephone)) {
+      hasError = true;
+      setFormErrors((prev) => ({ ...prev, telephone: true }));
+    }
+
+    if (hasError) return;
 
     const reCAPTCHAToken = await reCAPTCHARef.current?.executeAsync();
     reCAPTCHARef.current?.reset();
 
-    const response = await fetch('http://localhost:5500/api/contact', {
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL as string, {
       method: 'POST',
       body: JSON.stringify({
         reCAPTCHAToken,
-        name: contact_name.value,
-        email: contact_email.value,
-        telephone: contact_telephone.value,
-        city: contact_city.value,
-        subject: contact_subject.value,
-        content: contact_content.value,
+        name: name,
+        email: email,
+        telephone: telephone,
+        city: city,
+        subject: subject,
+        content: content,
       }),
     });
+
+    console.log(response);
   };
 
   return (
-    <div id={'contact'} className={styles.container}>
+    <div id='contact' className={styles.container}>
       <h2 className={styles.heading}>Fale Conosco!</h2>
       <form onSubmit={handleSubmit} className={styles.form}>
         <div className={styles.userDetailsWrapper}>
           <div className={styles.detail}>
-            <label className={styles.detailText} htmlFor={'contact_name'}>
-              Nome:
-            </label>
-            <input className={styles.detailContent} id={'contact_name'} type={'text'} required />
-          </div>
-          <div className={styles.detail}>
-            <label className={styles.detailText} htmlFor={'contact_email'}>
-              Email:
-            </label>
-            <input className={styles.detailContent} id={'contact_email'} type={'email'} required />
-          </div>
-          <div className={styles.detail}>
-            <label className={styles.detailText} htmlFor={'contact_telephone'}>
-              Telefone:
+            <label htmlFor='name' className={styles.detailText}>
+              Nome
             </label>
             <input
-              className={styles.detailContent}
-              id={'contact_telephone'}
-              type={'text'}
               required
+              id='name'
+              type='text'
+              value={formData.name}
+              onChange={handleChange}
+              className={styles.detailContent}
+            />
+            {formErrors.name && <span>Nome Errado</span>}
+          </div>
+          <div className={styles.detail}>
+            <label htmlFor='email' className={styles.detailText}>
+              Email
+            </label>
+            <input
+              required
+              id='email'
+              type='email'
+              onChange={handleChange}
+              value={formData.email}
+              className={styles.detailContent}
             />
           </div>
           <div className={styles.detail}>
-            <label className={styles.detailText} htmlFor={'contact_city'}>
-              Cidade:
+            <label htmlFor='telephone' className={styles.detailText}>
+              Tel.Celular
             </label>
-            <input className={styles.detailContent} id={'contact_city'} type={'text'} required />
+            <input
+              required
+              type='text'
+              pattern='\d*'
+              id='telephone'
+              onChange={handleChange}
+              value={formData.telephone}
+              className={styles.detailContent}
+              placeholder='Digite o número de telefone com DDD.'
+            />
+            {formErrors.telephone && <span>Telefone Errado</span>}
+          </div>
+          <div className={styles.detail}>
+            <label htmlFor='city' className={styles.detailText}>
+              Cidade
+            </label>
+            <select id='city' required onChange={handleChange} className={styles.detailContent}>
+              <option />
+              {cities.map((city) => {
+                return (
+                  <option value={city} key={city}>
+                    {city}
+                  </option>
+                );
+              })}
+            </select>
           </div>
         </div>
         <div className={styles.messageWrapper}>
-          <label className={styles.detailText} htmlFor={'contact_subject'}>
-            Assunto:
+          <label htmlFor='subject' className={styles.detailText}>
+            Assunto
           </label>
-          <select className={styles.detailContent} id={'contact_subject'} required>
-            <option>Contato</option>
-            <option>Voluntário</option>
+          <select id='subject' required onChange={handleChange} className={styles.detailContent}>
+            <option />
+            <option value='Contato'>Contato</option>
+            <option value='Voluntário'>Voluntário</option>
           </select>
-          <label className={styles.messageText} htmlFor={'contact_content'}>
-            Mensagem:
+          <label htmlFor='content' className={styles.messageText}>
+            Mensagem
           </label>
-          <textarea className={styles.messageContent} id={'contact_content'} required />
+          <textarea
+            required
+            id='content'
+            onChange={handleChange}
+            value={formData.content}
+            className={styles.messageContent}
+          />
+          {formErrors.content && <span>Falta Conteudo!</span>}
         </div>
         <div className={styles.recaptchaWrapper}>
           <ReCAPTCHA
-            badge={'inline'}
+            badge='inline'
+            size='invisible'
             ref={reCAPTCHARef}
-            size={'invisible'}
             sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string}
           />
           <button className={styles.button}>Enviar</button>
         </div>
-
         <span className={styles.bar}></span>
         <div className={styles.adressWrapper}>
           <div className={styles.adress}>
@@ -101,7 +197,7 @@ export const Contact: React.FC = () => {
             <p className={styles.text}>Rua 21, nº 34 - Vila Sta. Cecília</p>
             <p className={styles.text}>Volta Redonda / RJ - CEP: 27270-280</p>
             <p className={styles.text}>Telefax: (24) 3343-2049 / 3343-7262 / 3342-4885</p>
-            <a className={styles.mailto} href={'mailto:casadacrianca2@yahoo.com.br'}>
+            <a href='mailto:casadacrianca2@yahoo.com.br' className={styles.mailto}>
               casadacrianca2@yahoo.com.br
             </a>
           </div>
@@ -110,7 +206,7 @@ export const Contact: React.FC = () => {
             <p className={styles.text}>Rua Casemiro de Abreu, nº 07 - Centro</p>
             <p className={styles.text}>Nova Friburgo / RJ - CEP: 28613-050</p>
             <p className={styles.text}>Telefax: (22) 2533-1853</p>
-            <a className={styles.mailto} href={'mailto:casadacriancaedoadolescentenf@gmail.com'}>
+            <a href='mailto:casadacriancaedoadolescentenf@gmail.com' className={styles.mailto}>
               casadacriancaedoadolescentenf@gmail.com
             </a>
           </div>
