9e7277202ecffd40d8ffaaa646a6dd2e1f930f20
Renan Gomes <renango2015@gmail.com>
2023-07-24 04:08:03 -0300

    Update nodemailer configs

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
-  port: Number(process.env.EMAIL),
+  port: Number(process.env.PORT),
   host: process.env.HOST,
   secure: true,
   auth: {
