26fa1966dd275f2eb1d4315813a972ad9b5b5cf8
Renan Gomes <renango2015@gmail.com>
2023-08-03 11:41:43 -0300

    Update FAQ questions

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,52 +1,17 @@
 export const questions = [
   {
-    question: 'Lorem ipsum?',
+    question: 'Vocês tem criança para adoção?',
     answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum sed arcu non odio euismod lacinia at quis. Nibh cras pulvinar mattis nunc sed blandit libero volutpat. Vel orci porta non pulvinar. Turpis in eu mi bibendum. Viverra nam libero justo laoreet sit amet cursus. Amet purus gravida quis blandit turpis cursus in hac. Bibendum at varius vel pharetra vel turpis nunc eget lorem.',
+      'Não! Não somos abrigo. Procure o CRAS - Centro de Referência de Assistência Social mais perto de você e se informe sobre o cadastro de adoção. Sucesso!',
   },
   {
-    question: 'Lorem ipsum?',
+    question: 'Vocês aceitam doação de quê?',
     answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Semper feugiat nibh sed pulvinar proin gravida. Faucibus in ornare quam viverra orci sagittis. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel. Vitae nunc sed velit dignissim sodales ut eu.',
+      'Roupas e calçados para crianças(0 a 11 anos) e adolescentes (12 a 18 anos) e em bom estado, brinquedos em bom estado; roupas e acessórios para o Bazar, recursos financeiros. Outras doações fale conosco pelos números (24) 3342-5967 ou (24) 98839-7130.',
   },
   {
-    question: 'Lorem ipsum?',
+    question: 'Vocês recebem voluntário?',
     answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris pharetra et ultrices neque. Faucibus nisl tincidunt eget nullam non nisi est sit. Blandit cursus risus at ultrices mi tempus imperdiet nulla. Dolor magna eget est lorem ipsum dolor sit amet. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Tempor id eu nisl nunc. Elit ut aliquam purus sit amet. Ipsum faucibus vitae aliquet nec ullamcorper sit amet. Ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin.',
-  },
-  {
-    question: 'Lorem ipsum?',
-    answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat semper viverra nam libero justo. Fermentum posuere urna nec tincidunt praesent semper.',
-  },
-  {
-    question: 'Lorem ipsum?',
-    answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor lacus luctus accumsan tortor posuere ac ut consequat semper. Tincidunt lobortis feugiat vivamus at augue eget arcu. Odio pellentesque diam volutpat commodo sed egestas egestas. Dui ut ornare lectus sit amet est placerat in. Dolor sit amet consectetur adipiscing elit.',
-  },
-  {
-    question: 'Lorem ipsum?',
-    answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue. Felis imperdiet proin fermentum leo vel orci. Tortor at auctor urna nunc id cursus. Tellus mauris a diam maecenas sed enim ut sem. Facilisis magna etiam tempor orci eu lobortis elementum. Et magnis dis parturient montes nascetur.',
-  },
-  {
-    question: 'Lorem ipsum?',
-    answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada bibendum arcu vitae elementum curabitur vitae. Eget egestas purus viverra accumsan in nisl nisi. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Facilisis magna etiam tempor orci eu lobortis. Enim ut tellus elementum sagittis vitae. Eu tincidunt tortor aliquam nulla facilisi. Nec dui nunc mattis enim ut tellus elementum sagittis. Vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Amet aliquam id diam maecenas ultricies mi. Id faucibus nisl tincidunt eget nullam non nisi. Nunc eget lorem dolor sed viverra ipsum nunc.',
-  },
-  {
-    question: 'Lorem ipsum?',
-    answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac tortor vitae purus faucibus ornare.',
-  },
-  {
-    question: 'Lorem ipsum?',
-    answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra orci sagittis eu volutpat odio facilisis. Molestie nunc non blandit massa enim nec dui. Urna et pharetra pharetra massa massa. Vel eros donec ac odio tempor. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Rhoncus urna neque viverra justo nec ultrices dui sapien. Ultricies mi eget mauris pharetra et. Quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper.',
-  },
-  {
-    question: 'Lorem ipsum?',
-    answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel facilisis volutpat est velit egestas dui id ornare. Faucibus purus in massa tempor nec. Iaculis urna id volutpat lacus laoreet. Amet facilisis magna etiam tempor orci eu lobortis.',
+      'Sim! Venha nos visitar e saber como você pode ajudar – Rua 21, 34 - Vila Santa Cecilia, Volta Redonda – RJ.',
   },
 ];
