bd9f35a679c2727e2273c1b2b5e1296681602f3f
Renan Gomes <renango2015@gmail.com>
2023-08-03 11:40:12 -0300

    Update Home description

--- src/components/HomeComponents/Description.tsx
+++ src/components/HomeComponents/Description.tsx
@@ -5,22 +5,12 @@ export const Description: React.FC = () => {
     <div className={styles.container}>
       <h2 className={styles.heading}>Quem Somos?</h2>
       <p className={styles.text}>
-        A Casa da Criança e do Adolescente é uma organização da Sociedade Civil (OSC), fundada em
-        16/07/1996, de direito privado sem fins lucrativos, com Certificação de Entidade Beneficente
-        de Assistência Social. Atendemos 29 mil crianças e adolescentes de 0 a 18 anos por ano,
-        vítimas de diversas formas de violência. Atuamos em diversos programas sociais no estado do
-        Rio de Janeiro. A OSC da Casa da Criança e do Adolescente abrange a região ‘Médio Paraíba’
-        em 12 Municípios e ’Região Serrana’ com 12 Municípios. Acreditamos nos princípios
-        proclamados na Carta das Nações Unidas de que a liberdade, a justiça e a paz no mundo se
-        fundamentam no reconhecimento da dignidade inerente e dos direitos iguais e inalienáveis de
-        todos os membros da família humana; que a criança deve estar plenamente preparada para uma
-        vida independente na sociedade e deve ser educada de acordo com os ideais proclamados na
-        Carta, especialmente com Espírito de Paz, Dignidade, Tolerância, Liberdade, Igualdade e
-        Solidariedade. Temos como objetivo assegurar à criança e ao adolescente com absoluta
-        prioridade a GARANTIA DOS SEUS DIREITOS à vida, à saúde, à alimentação, à educação, à
-        moradia, ao lazer, à convivência familiar e comunitária DEFENDENDO-OS DE QUALQUER FORMA DE
-        NEGLIGÊNCIA, DISCRIMINAÇÃO E VIOLÊNCIA, através de Projetos e Programas nas áreas: social,
-        de saúde, cultura e educação.
+        A Casa da Criança e do Adolescente é uma associação civil, de direito privado, beneficente,
+        com fins econômicos e sem fins lucrativos, fundada em 16 de julho de 1996. É uma entidade de
+        atendimento, promoção, prevenção e proteção básica aas crianças, adolescentes e jovens.
+        Atendemos crianças e adolescentes vítimas de quaisquer forma de violência, assim como em
+        situação de risco ou vulnerabilidade social. A Casa da Criança e do Adolescente atende a 12
+        municípios do médio paraíba e 12 municípios da região serrana.
       </p>
     </div>
   );
