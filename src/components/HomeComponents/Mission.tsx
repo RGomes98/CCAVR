94ef880afab21c4d7abaa3c380edd611b3202ff4
Renan Gomes <renango2015@gmail.com>
2023-08-03 11:41:26 -0300

    Update Mission

--- src/components/HomeComponents/Mission.tsx
+++ src/components/HomeComponents/Mission.tsx
@@ -4,33 +4,84 @@ export const Mission: React.FC = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.heading}>Nossa Missão</h2>
-      <ul className={styles.textWrapper}>
-        <li className={styles.text}>
-          Ter segmentos com finalidade social, com o objetivo de manter os programas existentes ou
-          criar novos.
-        </li>
-        <li className={styles.text}>
-          Promover, com recursos próprios, parcerias, convênios e serviço voluntário, a realização
-          de seus objetivos; articulando, divulgando e informando sobre questões referentes à
-          criança e ao adolescente.
-        </li>
-        <li className={styles.text}>
-          A aplicação das receitas, rendas, rendimentos ou eventual resultado operacional da
-          entidade serão integralmente aplicados em território nacional, na manutenção e no
-          desenvolvimento de seus objetivos institucionais, de acordo com o art. 3º, inciso IV do
-          Decreto nº 2.536/98.
-        </li>
-        <li className={styles.text}>
-          Prestar serviços sem distinção de raça, credo religioso ou político, nacionalidade, sexo
-          ou qualquer outra forma de discriminação.
-        </li>
-      </ul>
-      <span className={styles.triangle} />
-      <span className={styles.triangle} />
-      <span className={styles.triangle} />
-      <span className={styles.triangle} />
-      <span className={styles.triangle} />
-      <span className={styles.triangle} />
+      <p className={styles.text}>
+        Acolher e atender de forma individual e sigilosa crianças e adolescentes encaminhadas pelos
+        órgãos do Sistema de Garantia de Direitos, de nossos outros programas e por procura
+        espontânea.
+      </p>
+      <div className={styles.wrapper}>
+        <div className={styles.missionWrapper}>
+          <span className={styles.missionHeading}>Visão</span>
+          <p className={styles.missionText}>
+            Ser referência na comunidade na qual está inserida e, ser reconhecida como a referência
+            nacional como uma Organização da Sociedade Civil que atua com crianças e adolescentes
+            vítimas de violência e em risco e vulnerabilidade social.
+          </p>
+        </div>
+        <div className={styles.missionWrapper}>
+          <span className={styles.missionHeading}>Valores</span>
+          <p className={styles.missionText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
+            aut dolores suscipit et amet nihil id laboriosam.
+          </p>
+        </div>
+        <div className={styles.missionWrapper}>
+          <span className={styles.missionHeading}>Acolhida</span>
+          <p className={styles.missionText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
+            aut dolores suscipit et amet nihil id laboriosam.
+          </p>
+        </div>
+        <div className={styles.missionWrapper}>
+          <span className={styles.missionHeading}>Cuidado</span>
+          <p className={styles.missionText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
+            aut dolores suscipit et amet nihil id laboriosam.
+          </p>
+        </div>
+        <div className={styles.missionWrapper}>
+          <span className={styles.missionHeading}>Personalização</span>
+          <p className={styles.missionText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
+            aut dolores suscipit et amet nihil id laboriosam.
+          </p>
+        </div>
+        <div className={styles.missionWrapper}>
+          <span className={styles.missionHeading}>Sustentabilidade</span>
+          <p className={styles.missionText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
+            aut dolores suscipit et amet nihil id laboriosam.
+          </p>
+        </div>
+        <div className={styles.missionWrapper}>
+          <span className={styles.missionHeading}>Ética</span>
+          <p className={styles.missionText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
+            aut dolores suscipit et amet nihil id laboriosam.
+          </p>
+        </div>
+        <div className={styles.missionWrapper}>
+          <span className={styles.missionHeading}>Transparência</span>
+          <p className={styles.missionText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
+            aut dolores suscipit et amet nihil id laboriosam.
+          </p>
+        </div>
+        <div className={styles.missionWrapper}>
+          <span className={styles.missionHeading}>Cultura de Paz</span>
+          <p className={styles.missionText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
+            aut dolores suscipit et amet nihil id laboriosam.
+          </p>
+        </div>
+        <div className={styles.missionWrapper}>
+          <span className={styles.missionHeading}>Respeito</span>
+          <p className={styles.missionText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
+            aut dolores suscipit et amet nihil id laboriosam.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
