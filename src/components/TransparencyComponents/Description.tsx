e12d28698166efcd7366b356c890c18106ff20f7
Renan Gomes <renango2015@gmail.com>
2023-08-03 11:41:58 -0300

    Update Transparency description

--- src/components/TransparencyComponents/Description.tsx
+++ src/components/TransparencyComponents/Description.tsx
@@ -8,7 +8,7 @@ export const Description: React.FC = () => {
         Nós da Casa da Criança e do Adolescente acreditamos que a transparência é fundamental para
         manter a confiança de nossos doadores e parceiros. Por isso, disponibilizamos uma área de
         transparência em nosso site, onde é possível ter acesso às informações financeiras e
-        prestação de contas de nossas atividades. Acreditamos que a transparência é a base de um
+        prestação de contas de nossas atividades. Cremos que a transparência é a base de um
         relacionamento saudável com nossos colaboradores e estamos comprometidos em manter essa
         prática em todas as nossas ações.
       </p>
