2db1ef02b02eee28a8e8092ffa0d71d453188cee
Renan Gomes <renango2015@gmail.com>
2023-08-03 21:04:22 -0300

    Update Projects names

--- src/data/projectDescription.ts
+++ src/data/projectDescription.ts
@@ -5,32 +5,32 @@ export const projectDescription = [
       'O programa NACA oferece atendimento a crianças e adolescentes, de 0 a 18 anos, em situação de violência intrafamiliar/doméstica sejam elas física, psicológica, sexual e negligência e a seus familiares, prestando atendimento psicológico, social e orientação jurídica, com vistas a proteção integral.',
   },
   {
-    name: 'PROGRAMA CURUMIM',
+    name: 'Programa Curumim',
     description:
       'O serviço de convivência e fortalecimento de vínculos Programa Curumim tem por objetivo trabalhar com crianças e adolescentes em situação de vulnerabilidade social, tendo como objetivo propiciar o desenvolvimento pessoal e social, realizando atividades nas áreas de educação, saúde, esporte, lazer e recreação, assegurando-lhes, ainda, por meio de ações integradas o atendimento de suas necessidades básicas.',
   },
   {
-    name: 'APOIO A FAMÍLIA',
+    name: 'Apoio a Família',
     description:
       'O Programa Apoio a Família objetiva atender as famílias cadastradas na Casa da Criança e do Adolescentes em suas necessidades básicas através de ações interventivas de uma equipe multidisciplinar. O programa também conta com uma equipe de voluntários, principalmente na área da saúde, que atendem as crianças e adolescentes acompanhadas pela instituição em seus consultórios particulares sem ônus para a família.',
   },
   {
-    name: 'AGA (ATENÇÃO A GESTANTE ADOLESCENTE)',
+    name: 'AGA (Atenção a Gestante Adolescente)',
     description:
       'O Programa AGA – Atenção a Adolescente Gestante, objetiva centralizar e oferecer um atendimento integral às adolescentes grávidas, facilitando o pré e pós-parto e buscando evitar uma segunda gravidez indesejada. O atendimento especial para as adolescentes grávidas é integrado as consultas de pré-natal e atividades dos grupos de orientação. São promovidas palestras com equipe multidisciplinar abordando questões sobre cuidados com a gestação e com o recém-nascido, além de estimular o aleitamento natural, diminuindo as ansiedades próprias desta etapa e orientando sobre os métodos contraceptivos. O programa acompanha a adolescente durante a gestação e após até a criança completar um ano de vida. O Programa AGA recebe adolescentes gestantes de acordo com os encaminhamentos dos programas da Casa da Criança e do Adolescente, Pastoral da Criança e demandas espontâneas.',
   },
   {
-    name: 'PROJETO CUIDAR',
+    name: 'Projeto Cuidar',
     description:
       'As famílias são encaminhadas após a avaliação técnica situacional realizada pelo Programa NACA; pelas Promotorias da Infância e Juventude; Varas da Infância e Juventude; Conselhos Tutelares; CREAS; DEAM. A intervenção proposta baseia-se na linha sistêmica, na presença da interdisciplinaridade das ações, com população-alvo de perfis diferenciados. A linha sistêmica possibilita a interlocução dos atores envolvidos no processo de avaliação e no acompanhamento biopsicossocial. As crianças/adolescentes vítimas são incluídos no serviço de acordo com critérios técnicos de prioridade, levando-se em consideração a gravidade de cada caso. Os familiares poderão ser incluídos nos atendimentos terapêuticos de família, e nos Grupos Terapêuticos Temáticos, que visam facilitar e motivar mudanças no padrão interrelacional das famílias, bem como no convívio familiar e comunitário; estimulando a reflexão através da informação, com múltiplos temas atuais inerentes ao processo de formação dos indivíduos e seus familiares, a inclusão cidadã e a humanização. A intervenção social realiza estudo do ambiente socioeconômico familiar; história de vida do usuário; composição familiar; local de moradia; religião, formação; profissão; situação empregatícia etc. São levantadas as vulnerabilidades existentes, procurando estabelecer propostas de tratamento social mais adequado e consciente, orientando os familiares sobre seus direitos e benefícios fornecidos por lei. Encontros dos técnicos com profissionais da rede socioassistencial dos municípios de origem dos usuários, fortalecendo e/ou criando espaço compartilhado de troca de informações e melhoria na qualidade dos serviços prestados.',
   },
   {
-    name: 'PROGRAMA IRIS',
+    name: 'Programa Iris',
     description:
       'O Programa IRIS desenvolve ações de prevenção secundária contra a violência doméstica e familiar através de equipe capacitada para Escuta Especializada, com avaliação, discussão de casos e articulação com a rede de atendimento do município de Nova Friburgo.',
   },
   {
-    name: 'PROGRAMA EVOLUIR',
+    name: 'Programa Evoluir',
     description:
       'O Programa EVOLUIR oferece acompanhamento familiar especializado para crianças, adolescentes e suas famílias; vítimas de violência doméstica e familiar. Conta ainda com Equipe de Prevenção a Violência contra crianças e adolescentes.',
   },
