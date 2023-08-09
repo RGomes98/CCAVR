2535a06ed8ee63ecd758cbf3adcb7cf0f867175d
Renan Gomes <renango2015@gmail.com>
2023-08-09 01:09:52 -0300

    Add links to News description

--- src/components/HomeComponents/Description.tsx
+++ src/components/HomeComponents/Description.tsx
@@ -1,10 +1,49 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import styles from '../../stylesheets/components/HomeComponentsStyles/Description.module.scss';
 
 export const Description: React.FC<{ heading: string; text: string }> = ({ heading, text }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  let usernameCount = 0;
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <div className={styles.container}>
       <h2 className={styles.heading}>{heading}</h2>
-      <p className={styles.text}>{text}</p>
+      {isMounted ? (
+        <p className={styles.text}>
+          {text.split('').map((char, idx) => {
+            if (char === '@') {
+              let current = idx + 1;
+              let username = '';
+
+              while (text[current] !== ' ' && current !== text.length - 1) {
+                username += text[current];
+                current++;
+              }
+
+              usernameCount = username.length;
+              return (
+                <a
+                  key={username}
+                  className={styles.userLink}
+                  href={`https://www.instagram.com/${username}`}
+                >
+                  @{username}
+                </a>
+              );
+            }
+
+            usernameCount--;
+            return usernameCount >= 0 ? [] : char;
+          })}
+        </p>
+      ) : null}
     </div>
   );
 };
