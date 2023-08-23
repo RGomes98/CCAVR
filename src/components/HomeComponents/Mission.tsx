5a86faf3d85e1fdbb2859accf8f10712c4103cf2
Renan Gomes <renango2015@gmail.com>
2023-08-23 10:12:45 -0300

    Update Mission

--- src/components/HomeComponents/Mission.tsx
+++ src/components/HomeComponents/Mission.tsx
@@ -10,78 +10,23 @@ export const Mission: React.FC = () => {
         espontânea.
       </p>
       <div className={styles.wrapper}>
-        <div className={styles.missionWrapper}>
-          <span className={styles.missionHeading}>Visão</span>
-          <p className={styles.missionText}>
-            Ser referência na comunidade na qual está inserida e, ser reconhecida como a referência
-            nacional como uma Organização da Sociedade Civil que atua com crianças e adolescentes
-            vítimas de violência e em risco e vulnerabilidade social.
-          </p>
-        </div>
-        <div className={styles.missionWrapper}>
-          <span className={styles.missionHeading}>Valores</span>
-          <p className={styles.missionText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
-            aut dolores suscipit et amet nihil id laboriosam.
-          </p>
-        </div>
-        <div className={styles.missionWrapper}>
-          <span className={styles.missionHeading}>Acolhida</span>
-          <p className={styles.missionText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
-            aut dolores suscipit et amet nihil id laboriosam.
-          </p>
-        </div>
-        <div className={styles.missionWrapper}>
-          <span className={styles.missionHeading}>Cuidado</span>
-          <p className={styles.missionText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
-            aut dolores suscipit et amet nihil id laboriosam.
-          </p>
-        </div>
-        <div className={styles.missionWrapper}>
-          <span className={styles.missionHeading}>Personalização</span>
-          <p className={styles.missionText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
-            aut dolores suscipit et amet nihil id laboriosam.
-          </p>
-        </div>
-        <div className={styles.missionWrapper}>
-          <span className={styles.missionHeading}>Sustentabilidade</span>
-          <p className={styles.missionText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
-            aut dolores suscipit et amet nihil id laboriosam.
-          </p>
-        </div>
-        <div className={styles.missionWrapper}>
-          <span className={styles.missionHeading}>Ética</span>
-          <p className={styles.missionText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
-            aut dolores suscipit et amet nihil id laboriosam.
-          </p>
-        </div>
-        <div className={styles.missionWrapper}>
-          <span className={styles.missionHeading}>Transparência</span>
-          <p className={styles.missionText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
-            aut dolores suscipit et amet nihil id laboriosam.
-          </p>
-        </div>
-        <div className={styles.missionWrapper}>
-          <span className={styles.missionHeading}>Cultura de Paz</span>
-          <p className={styles.missionText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
-            aut dolores suscipit et amet nihil id laboriosam.
-          </p>
-        </div>
-        <div className={styles.missionWrapper}>
-          <span className={styles.missionHeading}>Respeito</span>
-          <p className={styles.missionText}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim praesentium itaque natus
-            aut dolores suscipit et amet nihil id laboriosam.
-          </p>
-        </div>
+        <span className={styles.missionHeading}>Visão</span>
+        <span className={styles.missionHeading}>Valores</span>
+        <span className={styles.missionHeading}>Acolhida</span>
+        <span className={styles.missionHeading}>Cuidado</span>
+        <span className={styles.missionHeading}>Personalização</span>
+        <span className={styles.missionHeading}>Sustentabilidade</span>
+        <span className={styles.missionHeading}>Ética</span>
+        <span className={styles.missionHeading}>Transparência</span>
+        <span className={styles.missionHeading}>Cultura de Paz</span>
+        <span className={styles.missionHeading}>Respeito</span>
       </div>
+      <span className={styles.triangle} />
+      <span className={styles.triangle} />
+      <span className={styles.triangle} />
+      <span className={styles.triangle} />
+      <span className={styles.triangle} />
+      <span className={styles.triangle} />
     </div>
   );
 };
