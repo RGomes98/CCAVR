7eedf72aeab59ec755b332c4c0efc9ebbc565dea
Renan Gomes <renango2015@gmail.com>
2023-09-13 00:50:57 -0300

    Update NotFound page redirect

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,11 @@
+'use client';
+
 import { redirect } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function NotFound() {
-  return redirect('/');
+  useEffect(() => {
+    return redirect('/');
+  }, []);
 }
