a16928e1d7e5b27bf197aade6bf7425ed3b6d62f
Renan Gomes <renango2015@gmail.com>
2023-11-01 12:15:25 -0300

    Next redirect exception fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,9 +5,5 @@ import { isRedirectError } from 'next/dist/client/components/redirect';
 import { redirect } from 'next/navigation';
 
 export default function NotFound() {
-  try {
-    redirect('/');
-  } catch (error) {
-    if (isRedirectError(error)) throw error;
-  }
+  redirect('/');
 }
