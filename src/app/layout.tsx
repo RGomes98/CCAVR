417f97e5405f79270436797680a43f04d70ea2ee
Renan Gomes <renango2015@gmail.com>
2024-02-26 08:37:04 -0300

    refactor: update order

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,18 +10,18 @@ import { Fragment } from 'react';
 
 import '../stylesheets/global.scss';
 
+export const metadata = {
+  title: 'Casa da Criança e do Adolescente de Volta Redonda',
+  description:
+    'A Casa da Criança e do Adolescente é uma organização da Sociedade Civil (OSC), fundada em 16/07/1996, de direito privado sem fins lucrativos, com Certificação de Entidade Beneficente de Assistência Social.',
+};
+
 const poppins = Poppins({
   display: 'swap',
   subsets: ['latin'],
   fallback: ['system-ui', 'sans-serif'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
-
-export const metadata = {
-  title: 'Casa da Criança e do Adolescente de Volta Redonda',
-  description:
-    'A Casa da Criança e do Adolescente é uma organização da Sociedade Civil (OSC), fundada em 16/07/1996, de direito privado sem fins lucrativos, com Certificação de Entidade Beneficente de Assistência Social.',
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
