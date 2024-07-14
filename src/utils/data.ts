b042c57683ef3d4fd97102e22a984db1d6dff0a9
Renan Gomes <renango2015@gmail.com>
2024-07-14 10:12:13 -0300

    feat(utils): add data utility functions

---
+++ src/utils/data.ts
@@ -0,0 +1,29 @@
+import { SafeParseReturnType, ZodIssue } from 'zod';
+
+const extractFormDataFiles = (formData: Record<string, FormDataEntryValue>) => {
+  return Object.keys(formData)
+    .map((key) => formData[key])
+    .filter((key) => key instanceof File);
+};
+
+const extractFormDataErrors = <TData, TSafeParse extends SafeParseReturnType<TData, TData>>(
+  safeParseValues: TSafeParse[],
+  errorCallback: (errors: ZodIssue[]) => void
+) => {
+  const hasError = safeParseValues.some(({ error }) => {
+    if (error) errorCallback(error.errors);
+    return error;
+  });
+
+  return { hasError };
+};
+
+const formatErrors = (errors: ZodIssue[]) => {
+  return errors.reduce<Record<string, string>>((errors, { path, message }) => {
+    const [errorPath] = path;
+    errors[errorPath] = message;
+    return errors;
+  }, {});
+};
+
+export const data = { extractFormDataFiles, extractFormDataErrors, formatErrors };
